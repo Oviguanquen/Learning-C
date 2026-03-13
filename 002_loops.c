e94085250f06c31914d0012cc924d51d4f6d6ad5
Oviguanquen <oviguanquen@gmail.com>
2026-03-13 13:08:35 +0100

    loops

--- 002_loops.c
+++ 002_loops.c
@@ -5,6 +5,7 @@ int main(void)
     int sum, iterator;
 
     sum = 0;
+    // while(condition)
     while (sum < 100)
     {
         sum = sum + 1;
@@ -12,13 +13,15 @@ int main(void)
     printf ("%d\n", sum);
     
     sum = 0;
-    for (iterator = 0; sum < 100; iterator = iterator + 1)
+    // for(init; condition; step)
+    for (iterator = 0; iterator < 100; iterator = iterator + 1)
     {
         sum = sum + 1;
     }
     printf ("%d\n", sum);
 
     sum = 0;
+    // 1 Iteration - while(condition)
     do
     {
         sum = sum + 1;
