1ca0d71e5a8d4bebae2e2cc67bf6982fe171ce9e
Oviguanquen <oviguanquen@gmail.com>
2026-03-13 18:35:39 +0100

    00-ReverseString

---
+++ 00-ReverseString.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+void			f_reverse(char* string);
+void			f_print(char* string);
+unsigned int	f_len(char* string);
+
+int main(void)
+{
+	char string [] = "test.qw";
+
+	f_print(string);
+
+	f_reverse(string);
+
+	f_print(string);
+
+	return 0;
+}
+
+void f_reverse(char* string)
+{
+	unsigned int 	len	= f_len(string);
+	unsigned int	i = 0;
+	unsigned int	j = len - 1;
+	char			tmp;
+
+	while(i < j)
+	{
+		tmp			= string[i];
+		string[i]	= string[j];
+		string[j]	= tmp;
+		i++;
+		j--;
+	}
+}
+
+void f_print(char* string)
+{
+	unsigned int len	= f_len(string);
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+	{
+		putchar(string[i]);
+	}
+
+	putchar('\n');
+}
+
+unsigned int f_len(char* string)
+{
+	unsigned int len	= 0;
+
+	while(string[len] != '\0')
+	{
+		len++;
+	}
+
+	return len;
+}
